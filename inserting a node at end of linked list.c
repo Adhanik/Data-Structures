9dcbf35fa9e5427af81f301406a5b6d12be431f8
AMIT DHANIK <32511626+Adhanik@users.noreply.github.com>
2018-09-19 16:36:51 +0000

    Add files via upload
    linked list

---
+++ inserting a node at end of linked list.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node* link;
+};
+
+struct Node* root =NULL;
+void append()
+{
+    struct Node* temp;
+    temp = (struct Node*)malloc(sizeof(struct Node));//a new node is created everytime dynamically and the user is then asked to enter the data AND LINK IN NODE ,THEN IT CHECKS WHETHER THE LINK IS ROOT NODE OR NOT.
+    printf("enter the data \n");
+    scanf("%d",&temp->data);
+    temp->link=NULL;
+    if(root == NULL)
+    {
+        root=temp;
+    }
+    else
+    {
+        struct Node* p;
+        p=root;
+        while(p->link != NULL)
+        {
+            p=p->link;
+        }
+        p->link=temp;
+        
+    }
+    
+}
+int main()
+{
+    append();
+    return 0;
+}
